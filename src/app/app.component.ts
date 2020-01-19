8bacc7fb888e77b0a2daae8b97c4e0522d7afa23
Lukas Spirig <lukas.spirig@gmail.com>
2020-01-19 01:00:32 +0100

    feat: minor usage improvements in root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostBinding } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { TranslationService } from './core/translation.service';
 
@@ -10,7 +11,16 @@ import { TranslationService } from './core/translation.service';
 export class AppComponent {
   @HostBinding('class.service-down') serviceDown = false;
 
-  constructor(translationService: TranslationService) {
-    translationService.serviceDown.subscribe(a => (this.serviceDown = a));
+  constructor(translationService: TranslationService, snackbar: MatSnackBar) {
+    translationService.serviceDown.subscribe(a => {
+      this.serviceDown = a;
+      if (a) {
+        snackbar.open(
+          `Translation server is not available. Start the server in the console with the command 'ng run yourProject:t9n'`,
+          undefined,
+          { duration: 2500 }
+        );
+      }
+    });
   }
 }
