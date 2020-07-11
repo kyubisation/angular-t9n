009fd8ab27574ccaa8a549c4301e3b37da6cdc5d
Lukas Spirig <lukas.spirig@gmail.com>
2020-07-11 17:13:29 +0200

    fix: linting

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -52,6 +52,7 @@
  *
  */
 
+// tslint:disable
 /***************************************************************************************************
  * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
  */
