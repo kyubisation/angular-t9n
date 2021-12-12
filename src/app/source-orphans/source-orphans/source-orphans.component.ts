6a498fa5237e178eb9e90b605a3fe0c9c0db2a24
Lukas Spirig <lukas.spirig@gmail.com>
2021-12-12 22:39:45 +0100

    chore: add eslint

--- src/app/source-orphans/source-orphans/source-orphans.component.ts
+++ src/app/source-orphans/source-orphans/source-orphans.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 't9n-source-orphans',
@@ -6,8 +6,4 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   styleUrls: ['./source-orphans.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SourceOrphansComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class SourceOrphansComponent {}
